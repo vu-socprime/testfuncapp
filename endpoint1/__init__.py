e820016e2fbe3228a5004b7940a52ca10c6318d0
Vitalii Uslystyi <vu@socprime.com>
2022-06-16 18:24:27 +0300

    initial commit

---
+++ endpoint1/__init__.py
@@ -0,0 +1,7 @@
+import logging
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse('test response')
