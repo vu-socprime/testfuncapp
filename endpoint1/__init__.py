7353f84acf0f1442aff48d95e447a38dd1bba23d
Vitalii Uslystyi <vu@socprime.com>
2022-06-16 18:37:00 +0300

    add requests

--- endpoint1/__init__.py
+++ endpoint1/__init__.py
@@ -1,7 +1,9 @@
 import logging
 import azure.functions as func
+import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    res = requests.get('https://google.com')
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse('test response')
+    return func.HttpResponse(f'test response {res.status_code}')
