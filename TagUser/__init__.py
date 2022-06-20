81149e9547ea401d8f0070dbda5108de1c2cf909
Vitalii Uslystyi <vu@socprime.com>
2022-06-20 10:43:45 +0300

    update

--- TagUser/__init__.py
+++ TagUser/__init__.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import azure.functions as func
 import boto3
@@ -6,28 +5,29 @@ from botocore.exceptions import ClientError
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    username = req.params.get('username')
-    tag = req.params.get('tag')
-    if username and tag:
-        logging.getLogger().setLevel(logging.INFO)
-        logging.info('Starting program')
+    username = req.params.get("username")
+    tag_key = req.params.get("tag_key")
+    tag_value = req.params.get("tag_value")
+    if username and tag_key and tag_value:
         try:
-            iam = boto3.client('iam',
-                               aws_access_key_id=os.environ["AWS_AccessKeyId"],
-                               aws_secret_access_key=os.environ["AWS_SecretAccessKey"])
+            iam = boto3.client(
+                "iam",
+                aws_access_key_id=os.environ["AWS_AccessKeyId"],
+                aws_secret_access_key=os.environ["AWS_SecretAccessKey"],
+            )
             try:
-                iam.tag_user(UserName=username, Tags=[tag])
-            except iam.exceptions.NoSuchEntityException or iam.exceptions.LimitExceededException or \
-                   iam.exceptions.InvalidInputException as err:
+                iam.tag_user(
+                    UserName=username, Tags=[{"Key": tag_key, "Value": tag_value}]
+                )
+            except iam.exceptions.NoSuchEntityException as err:
+                return func.HttpResponse(str(err), status_code=404)
+            except iam.exceptions.InvalidInputException as err:
                 return func.HttpResponse(str(err), status_code=400)
-            except iam.exceptions.ServiceFailureException or iam.exceptions.ConcurrentModificationException as err:
-                return func.HttpResponse(str(err), status_code=500)
             return func.HttpResponse("Successfully added tags.", status_code=200)
         except ClientError as err:
-            return func.HttpResponse(str(err), status_code=400)
+            return func.HttpResponse(str(err), status_code=401)
     else:
         return func.HttpResponse(
-            "Please pass a username and tags on the query string",
-            status_code=400
+            "Please pass a username and tag_key and tag_value on the query string",
+            status_code=400,
         )
